521fafee10b867aeabed877a0d6e0160f19b7545
John Rachid <jrachid@iastate.edu>
2019-10-17 16:13:31 -0500

    Fixed spacing

--- checkers/main.py
+++ checkers/main.py
@@ -12,38 +12,40 @@ def main():
     game.consecutive_noncapture_move_limit = 100
     while (not game.is_over()):
         if game.whose_turn() == 1:
-            human_move(game) #if you want the bot to play for the human replace this line with bot_move(game,desired_depth)
+            human_move(
+                game)  # if you want the bot to play for the human replace this line with bot_move(game,desired_depth)
             # bot_move(game, 1)
         else:
-            bot_move(game,5)
+            bot_move(game, 5)
         print_game_to_console(game)
 
 
 def human_move(game):
     print("Possible moves for human ", game.get_possible_moves())
-    prompt ="insert move number from list 0 - " + str(len(game.get_possible_moves())-1)
+    prompt = "insert move number from list 0 - " + str(len(game.get_possible_moves()) - 1)
     move_number = int(input(prompt))
     if move_number >= len(game.get_possible_moves()):
-        print("number out of bounds please provide a number within 0 - ", len(game.get_possible_moves())-1)
+        print("number out of bounds please provide a number within 0 - ", len(game.get_possible_moves()) - 1)
         move_number = int(input(prompt))
 
     print("player moved to ", game.get_possible_moves()[move_number])
     game.move(game.get_possible_moves()[move_number])
 
 
-def bot_move(game,depth):
+def bot_move(game, depth):
     print("Possible moves for bot ", game.get_possible_moves())
     first_moves = []
 
     for i in range(0, len(game.get_possible_moves())):
         new_game = copy.deepcopy(game)
         new_game.move(game.get_possible_moves()[i])
-        val,best_move = alphabeta(new_game, depth, float("-inf"), float("inf"), True,game.get_possible_moves()[0])
+        val, best_move = alphabeta(new_game, depth, float("-inf"), float("inf"), True, game.get_possible_moves()[0])
         # print(val)
         first_moves.append(val)
     index, value = max(enumerate(first_moves), key=operator.itemgetter(1))
-    duplicates = [i for i, x in enumerate(first_moves) if x == value]#isint python just the best?
-    print("bot moved to ", game.get_possible_moves()[random.choice(duplicates)])#picks randomly between the elements with the highest values
+    duplicates = [i for i, x in enumerate(first_moves) if x == value]  # isint python just the best?
+    print("bot moved to ", game.get_possible_moves()[
+        random.choice(duplicates)])  # picks randomly between the elements with the highest values
 
     # print(val)
     game.move(game.get_possible_moves()[index])
@@ -59,7 +61,7 @@ def alphabeta(node, depth, alpha, beta, maximizingPlayer,
         for i in range(0, len(node.get_possible_moves())):
             new_game = copy.deepcopy(node)
             new_game.move(node.get_possible_moves()[i])
-            new_value,best_move = alphabeta(new_game, depth - 1, alpha, beta, False, best_move)
+            new_value, best_move = alphabeta(new_game, depth - 1, alpha, beta, False, best_move)
 
             value = max(value, new_value)
             if get_game_score(new_game) > value:
@@ -74,9 +76,9 @@ def alphabeta(node, depth, alpha, beta, maximizingPlayer,
         for i in range(0, len(node.get_possible_moves())):
             new_game = copy.deepcopy(node)
             new_game.move(node.get_possible_moves()[i])
-            new_value,best_move = alphabeta(new_game, depth - 1, alpha, beta, True, best_move)
+            new_value, best_move = alphabeta(new_game, depth - 1, alpha, beta, True, best_move)
 
-            value = min(value, new_value)#need to do the same as above here
+            value = min(value, new_value)  # need to do the same as above here
             beta = min(beta, value)
             if get_game_score(new_game) < value:
                 best_move = node.get_possible_moves()[i]
